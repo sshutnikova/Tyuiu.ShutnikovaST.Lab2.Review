211f5fd0943d3e4e164cadaa8baa9fec804918f6
BBC_User <BBC_User@BBC-LAPTOP1>
2024-12-03 22:02:56 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ShutnikovaST.Lab2.Review.V12.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿namespace Tyuiu.ShutnikovaST.Lab2.Review.V12.Lib
+{
+    public class DataService
+    {
+        public double[] GetMassFunction(int start, int end)
+        {
+            if (start > end)
+                return new double[0];
+            double[] results = new double[end - start + 1];
+            int index = 0;
+            for (int x = start; x <= end; x++)
+            {
+                double znamenatel = Math.PI - 2 * Math.Pow(x, 3);
+                if (Math.Abs(znamenatel) < 0.000001) 
+                {
+                    results[index] = double.NaN; 
+                }
+                else
+                {
+                    results[index] = Math.Cos(x) / znamenatel + 16 * x * Math.Cos(x) - 2; 
+                    results[index] = Math.Round(results[index], 2); 
+                }
+                index++; 
+            }
+            return results;
+        }
+    }
+}
---
+++ Tyuiu.ShutnikovaST.Lab2.TaskReview.V12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Tyuiu.ShutnikovaST.Lab2.Review.V12.Lib;
+
+namespace Tyuiu.ShutnikovaST.Lab2.TaskReview.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = " Выполнила: Шутникова С.Т. | МКМб-22-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("******************************************************************************");
+            Console.Write("Старт шага = ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Конец шага = ");
+            int end = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    f(x)  |");
+            Console.WriteLine("+----------+----------+");
+            double[] results = ds.GetMassFunction(start, end);
+            for (int x = 0; x < results.Length; x++)
+            {
+                int i = start + x;
+                string fx = results[x].ToString();
+                Console.WriteLine($"| {i,8} | {fx,8} |");
+            }
+            Console.ReadKey();
+        }
+    }
+}
